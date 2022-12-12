15e6f1cd4077987a1ef94b9f3625bfa350900a17
Draconic1 <269686@mail.ru>
2022-12-12 20:11:01 +0300

    add sonya.sql

---
+++ sonya.sql
@@ -0,0 +1,33 @@
+use flower;
+
+INSERT categories(name, image, createdAt, updatedAt) 
+VALUES ('Букеты на свадьбу', 'https://s1.hostingkartinok.com/uploads/images/2022/12/ab1259e80b0041a2fa5cbd800d3e2e5d.png', '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT categories(name, image, createdAt, updatedAt) 
+VALUES ('Букеты на День Рождения', 'https://i.postimg.cc/0Nb5RbTF/image.png', '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT categories(name, image, createdAt, updatedAt) 
+VALUES ('Авторские букеты', 'https://s1.hostingkartinok.com/uploads/images/2022/12/979f522d824df23fe0f92d2c4fd64da9.png', '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT categories(name, image, createdAt, updatedAt) 
+VALUES ('Цветы поштучно', 'https://s1.hostingkartinok.com/uploads/images/2022/12/8aecce248429357777af6ae72c859d3d.png', '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет 21 кустовой хризантемы микс', 3500, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/9b9/242_242_0/4.jpg', 2, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет 25 хризантем кустовых', 4500, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/a47/242_242_0/7.jpg', 2, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет 11 белых хризантем', 2260, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/eee/242_242_0/57.1.jpg', 2, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет из 5 розовых кустовых хризантем', 1650, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/aba/242_242_0/2.jpg', 1, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет из 5 белых кустовых хризантем', 1650, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/695/242_242_0/5.jpg', 1, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет 17 гербер и гипсофил', 5316, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/121/242_242_0/3.jpg', 1, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет 25 красных роз', 5030, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/787/242_242_0/13.jpg', 1, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет 1 белой гортензии', 500, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/6e7/242_242_0/IMG_0147.jpg', 4, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет из 1 красной гортензии', 500, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/94d/242_242_0/IMG_0132.jpg', 4, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет 7 белых гипсофил', 2790, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/4eb/242_242_0/1.jpg', 3, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
+INSERT products(name, price, image, category_id, createdAt, updatedAt) 
+VALUES ('Букет из 101 розы', 14000, 'https://tsvet-ryad.ru/upload/resize_cache/iblock/802/242_242_0/karmel_trio_1_.jpg', 3, '2022-12-11 18:26:00', '2022-12-11 18:26:00');
